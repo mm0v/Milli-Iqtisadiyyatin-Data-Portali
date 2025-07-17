d9b49fd3819fb5ef04057359b6daadbeec563667
ayxan-mirzeyev-7674 <amirzeyev3@gmail.com>
2025-07-17 21:08:20 +0400

    Help and Contact Cards

--- src/pages/HelpAndContact.tsx
+++ src/pages/HelpAndContact.tsx
@@ -1,28 +1,34 @@
-import Navbar from "@/layout/Navbar"
-import "../assets/sass/helpandcontact.scss"
+import Navbar from "@/layout/Navbar";
+import "../assets/sass/helpandcontact.scss";
+import bgVideo from "../../public/helpAndContact/bg-helpandcontact.mp4";
+import HelpAndContactCard from "@/components/HelpAndContactCard";
+import { useTranslation } from "react-i18next";
 
 const HelpAndContact = () => {
-    return (
-        <div>
-            <div>
-                {/* <video
-                    autoPlay
-                    loop
-                    muted
-                    className="absolute top-0 left-0 w-full h-full object-cover -z-10"
-                    style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%' }}
-                >
-                    <source src="./about/bg-about.mp4" type="video/mp4" />
-                </video> */}
-                <div className="absolute top-0 left-0 w-full h-full object-cover -z-10"
-                    style={{ top: 0, left: 0, width: '100%', height: '100%', backgroundColor: '#050412' }}>
-                </div>
-            </div>
-            <div className="absolute bottom-0">
-                <Navbar />
-            </div>
-        </div>
-    )
-}
+  const { t } = useTranslation();
+  return (
+    <div className="">
+      <div className="fixed inset-0 -z-10">
+        <video
+          className="w-full h-full object-cover bg-video"
+          autoPlay
+          loop
+          muted
+        >
+          <source src={bgVideo} type="video/mp4" />
+        </video>
+        <div className="absolute inset-0 bg-[#1D0416] opacity-80" />
+      </div>
+      <div className="absolute bottom-0">
+        <Navbar />
+      </div>
+      <div className="absolute right-0 top-30 right-10 flex gap-10.5">
+        <HelpAndContactCard title={t("faq")} to={null} />
+        <HelpAndContactCard title={t("user_instructions")} to={null} />
+        <HelpAndContactCard title={t("contact_form")} to={null} />
+      </div>
+    </div>
+  );
+};
 
-export default HelpAndContact
+export default HelpAndContact;
