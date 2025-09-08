c0d1010daf429a26d577b96a76d69d1190c7d610
Rasid <farzrash.rf@gmail.com>
2025-09-08 10:34:30 +0400

    Fix cards int /report

--- src/pages/Report.tsx
+++ src/pages/Report.tsx
@@ -1,67 +1,46 @@
-import { useNavigate } from "react-router-dom";
-import IconAccordion from "/report/IconAccordion.png"
+import { useMatch, useNavigate } from "react-router-dom";
+import IconAccordion from "/report/IconAccordion.png";
 import BackgroundVideo from "@/components/bg-video/BackgroundVideo";
 import { useTranslation } from "react-i18next";
-
+import LinearButton from "@/components/ui/linearButton";
+import HelpSidebar from "@/components/HelpSidebar";
 
 const Report = () => {
-    const { t } = useTranslation()
+  const { t } = useTranslation();
+  const isButton = useMatch("/report");
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleClickAcademicReport = () => {
-        navigate('/academic_reports');
-    };
+  const handleClickAcademicReport = () => {
+    navigate("/academic_reports");
+  };
 
-    const handleClickAcademicPublications = () => {
-        navigate('/academic_publications');
-    };
+  const handleClickAcademicPublications = () => {
+    navigate("/academic_publications");
+  };
 
-    return (
-        <div className="relative">
-            <div className="video-background">
-                <BackgroundVideo videoSrc="/about/bg-about.mp4" />
-            </div>
+  return (
+    <div className="help-and-contact flex flex-col lg:flex-row">
+      <div className="video-background">
+        <BackgroundVideo videoSrc="/about/bg-about.mp4" />
+      </div>
 
-            <div className="relative">
-                <div className="flex justify-end p-4 flex-wrap">
-                    <div onClick={handleClickAcademicReport} className="m-1 w-84">
-                        <div className=" cursor-pointer mb-4 mt-20 rounded-[8px]"
-                        >
-                            <div className="relative">
-                                <div className="w-full">
-                                    <BackgroundVideo videoSrc="/report/akademic.mp4" />
-                                </div>
-                                <div className="absolute bottom-4">
-                                    <p className="text-white text-2xl mt-2 ml-2">{t('report.publications')}</p>
-                                </div>
-                                <div className="absolute top-0 right-0 rotate-270 p-2">
-                                    <img className='w-7' src={IconAccordion} alt="IconAccordion" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div onClick={handleClickAcademicPublications} className="m-1 w-84">
-                        <div className=" cursor-pointer mb-4 mt-20 rounded-[8px]"
-                        >
-                            <div className="relative">
-                                <div className="w-full">
-                                    <BackgroundVideo videoSrc="/report/akademic.mp4" />
-                                </div>
-                                <div className="absolute bottom-4">
-                                    <p className="text-white text-2xl mt-2 ml-2">{t('report.reports')}</p>
-                                </div>
-                                <div className="absolute top-0 right-0 rotate-270 p-2">
-                                    <img className='w-7' src={IconAccordion} alt="IconAccordion" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+      {isButton ? (
+        <div className="absolute top-[60px] md:right-10 grid grid-cols-1 md:grid-cols-2 grid-rows-2 gap-6 w-full lg:max-w-[800px] items-center justify-center p-10 md:p-[60px]">
+          <LinearButton
+            title={t("report.publications")}
+            pageRouter="/academic_reports"
+          />
+          <LinearButton
+            title={t("report.reports")}
+            pageRouter="/academic_publications"
+          />
         </div>
-    );
+      ) : (
+        <HelpSidebar />
+      )}
+    </div>
+  );
 };
 
 export default Report;
